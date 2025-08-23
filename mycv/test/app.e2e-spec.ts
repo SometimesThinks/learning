b6fcd1057996b12a6f1227e9d88686ffff8dfde4
SometimesThinks <ycw2859@gmail.com>
2025-08-24 02:11:10 +0900

    add: 앱 구성 관리 학습

--- mycv/test/app.e2e-spec.ts
+++ mycv/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
@@ -21,5 +21,23 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'test@test.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '12345' })
+      .expect(201);
+
+    const cokie = res.headers['set-cookie'];
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cokie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
