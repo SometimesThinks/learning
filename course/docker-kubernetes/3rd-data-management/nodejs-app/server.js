a3332ad03772afa0b2b494e165eb0efea4433ac7
SometimesThinks <ycw2859@gmail.com>
2025-12-13 18:12:52 +0900

    docker: 볼륨&바인드 마운트 추가 내용 학습

--- course/docker-kubernetes/3rd-data-management/nodejs-app/server.js
+++ course/docker-kubernetes/3rd-data-management/nodejs-app/server.js
@@ -28,8 +28,6 @@ app.post('/create', async (req, res) => {
 
   const adjTitle = title.toLowerCase();
 
-  console.log('TEST!!!!');
-
   const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
   const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
 
@@ -45,4 +43,4 @@ app.post('/create', async (req, res) => {
   });
 });
 
-app.listen(80);
+app.listen(process.env.PORT);
