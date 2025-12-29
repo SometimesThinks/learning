05e0b676da454927cbb8c7914fbd01a21a30514c
SometimesThinks <ycw2859@gmail.com>
2025-12-29 11:04:05 +0900

    Squashed 'course/nextjs/' content from commit e5602c3
    git-subtree-dir: course/nextjs
    git-subtree-split: e5602c3cbbc292923bd61febc038c5c345ddb13b

---
+++ 02-nextjs-essentials/lib/actions.js
@@ -0,0 +1,39 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+import { saveMeal } from './meals';
+
+function isInvalidMeal(text) {
+  return !text || text.trim() === '';
+}
+
+export async function shareMeal(prevState, formData) {
+  const meal = {
+    title: formData.get('title'),
+    summary: formData.get('summary'),
+    instructions: formData.get('instructions'),
+    image: formData.get('image'),
+    creator: formData.get('name'),
+    creator_email: formData.get('email'),
+  };
+
+  if (
+    isInvalidMeal(meal.title) ||
+    isInvalidMeal(meal.summary) ||
+    isInvalidMeal(meal.instructions) ||
+    isInvalidMeal(meal.creator) ||
+    isInvalidMeal(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: 'Invalid input',
+    };
+  }
+
+  await saveMeal(meal);
+  revalidatePath('/meals');
+  redirect('/meals');
+}
