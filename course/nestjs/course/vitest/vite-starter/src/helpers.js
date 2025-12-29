b275dd0f7ad1c055c0b7997607d54b7024133b30
SometimesThinks <ycw2859@gmail.com>
2025-12-29 10:13:11 +0900

    delete: 이상하게 병합된 nestjs 폴더 삭제

--- course/nestjs/course/vitest/vite-starter/src/helpers.js
+++
@@ -1,7 +0,0 @@
-export const kebabCaseToTitleCase = (colorName) => {
-  const colorWithSpaces = colorName.replaceAll('-', ' ');
-  const colorWithCaps = colorWithSpaces.replace(/\b([a-z])/g, (match) =>
-    match.toUpperCase()
-  );
-  return colorWithCaps;
-};
